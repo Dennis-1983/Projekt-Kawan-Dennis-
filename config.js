023680a23a1cd881d54f9b417adcc664ccc01480
Dennisbullys <d.rochus@hotmail.de>
2022-05-04 14:21:02 +0200

    DennisHunde

---
+++ config.js
@@ -0,0 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
+export const localConnection = "mongodb://localhost:27017/practice-1";
+export const remoteConnection =
+  "mongodb+srv://DennisDci23:1Milapie@cluster0.tx7js.mongodb.net/Dogo?retryWrites=true&w=majority";
---
+++ index1.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+const userSchema = new mongoose.Schema({
+  rasse: String,
+  farbe: String,
+  alter: Number,
+  gender: String,
+  ort: String,
+  preis: Number,
+  name:String
+});
+
+const User = mongoose.model("User", userSchema);
+
+
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find().exec();
+    return res.json(users);
+  } catch (error) {
+    return res.json({ error: error.message });
+  }
+});
+
+app.post("/addUsers", async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    await newUser.save();
+    return res.json(newUser);
+  } catch (error) {
+    return res.json({ error: error.message });
+  }
+});
+
+
+
+
+
+
+
+
+mongoose.connect(process.env.REMOTE_CONNECTION).then(() => {
+  app.listen(port, () => {
+    console.log(`appExample app listening on port ${port}`);
+  });
+});
+
+
+
