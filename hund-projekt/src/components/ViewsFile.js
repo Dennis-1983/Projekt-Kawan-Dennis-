6a4b672f9b001c897d627e849282f4210ec14fbe
kawan <kawan@kawan-linux.local>
2022-05-04 11:13:25 +0200

    new update

--- hund-projekt/src/components/ViewsFile.js
+++ hund-projekt/src/components/ViewsFile.js
@@ -3,15 +3,13 @@ import { useState } from 'react';
 
 export const ViewsFile = () => {
   const [data, setdata] = useState([]);
-const hunde=data.map((data,index)=>{data})
+  const hunde = data.map((data, index) => {
+    data;
+  });
   return (
-
     <div className="data">
-    return(
-         <ul>
-              {data.map(data)}
-         </ul>
-    )
+      return(
+      <ul>{data.map(hunde)}</ul>)
     </div>
   );
 };
